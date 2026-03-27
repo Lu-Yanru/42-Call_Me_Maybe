1a4669a66e0829cb4d969764ad1e753589be0513
Yanru Lu <yanlu@c3a3c10.42berlin.de>
2026-03-27 12:44:58 +0100

    sned prompt and get response using greedy decoding

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+import os
+from timeit import default_timer as timer
+
+
 from src.parse_args import parse_args
 from src.parse_funcs import parse_funcs, FuncDefError
 from src.parse_prompts import parse_prompts, PromptError
@@ -9,6 +14,11 @@ def main() -> None:
     # parse CLI
     args = parse_args()
 
+    # load HF token
+    if os.path.exists(".env"):
+        load_dotenv()
+
+    start = timer()
     try:
         # parse functions definitions
         funcs = parse_funcs(args.functions_definition)
@@ -25,6 +35,9 @@ def main() -> None:
         print(e)
     except OSError as e:
         print(e)
+    finally:
+        end = timer()
+        print(f"Execution time: {end - start:.2f}s")
 
 
 if __name__ == "__main__":
--- src/process_prompt.py
+++ src/process_prompt.py
@@ -22,10 +22,31 @@ class PromptProcessor:
             prompt_output: dict[str, str | dict[str, Any]] = {}
 
             prompt_output["output"] = prompt.prompt
+            prompt_output["response"] = self.generate(prompt)
             # prompt_output["name"] = self.generate_func_name(prompt)
             # prompt_output["parameters"] = self.generate_parameters(prompt)
 
             self.output.append(prompt_output)
+
+    def get_next_token_id(self, input_ids: list[int]) -> int:
+        # Get a list of logits of the input ids
+        logits = self.llm.get_logits_from_input_ids(input_ids)
+
+        # Get the input id with the max logit
+        return max(enumerate(logits), key=lambda x: x[1])[0]
+
+    def generate(self, prompt: Prompt) -> str:
+        # Encode prompt into a 2D tensor and convert into a list
+        input_ids = self.llm.encode(prompt.prompt).squeeze(0).tolist()
+
+        generated_ids: list[int] = []
+
+        for _ in range(10):
+            # Feed the full ids into the llm
+            next_id = self.get_next_token_id(input_ids + generated_ids)
+            generated_ids.append(next_id)
+
+        return self.llm.decode(generated_ids)
 
     # def generate_func_name(self, prompt: Prompt) -> str:
     #     pass
